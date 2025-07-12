ac6fd6302677e8a72a4d059ee8b2b96e0d35874f
Sunny Gupta <vknow360@gmail.com>
2025-07-12 19:42:45 +0530

    Fix HTTP method for root route handler in index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', async (req,res) => {
+app.get('/', (req,res) => {
     res.status(200).json({ status: 'OK' });
 });
 
