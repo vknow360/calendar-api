9c39db2982dbc42ac3ff71ce797d2b41deab27af
Sunny Gupta <vknow360@gmail.com>
2025-07-15 20:06:57 +0530

    Enhance file upload functionality in calendar route with error handling and session management

--- routes/calendar.route.js
+++ routes/calendar.route.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const fs = require("fs");
 const axios = require("axios");
-
+const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
 const router = express.Router();
@@ -37,19 +36,44 @@ router.get("/", async (req, res) => {
 
 router.post("/upload", upload.single("file"), async (req, res) => {
   try {
+    const batch = req.body.course;
+    const session = req.body.session;
+    console.log("Batch:", batch);
+    console.log("Session:", session);
     const fileName = req.file.originalname;
 
     const base64 = req.file.buffer.toString("base64");
-
-    const response = await axios.post("https://script.google.com/macros/s/AKfycbycoMpeGVr14LHh1yZc92BXSZk8kuRhpF4Mv0mi3whOqyRAg_ZXP5mAa7kQfmO1EDX5jw/exec", null, {
-      params: {
+    const response = await axios.post(
+      "https://script.google.com/macros/s/AKfycbyVPuL1D31vqp0zPfjhBzEo0JyWy_EOujk3Pf-YWTJpt2bliFH8C0yEdiJvJ-2AwIMtCw/exec",
+      {
         name: fileName,
         data: base64,
-      },
+      }
+    );
+
+    if (response.data.status !== "success") {
+      return res.status(500).json({
+        status: "error",
+        message: "Failed to upload academic calendar",
+      });
+    }
+    const fileUrl = response.data.previewUrl;
+    const existingRecord = await Calendar.findOne({ course: batch });
+    if (existingRecord) {
+      existingRecord.sessions.set(session, fileUrl);
+      await existingRecord.save();
+    } else {
+      const newRecord = new Calendar({
+        course: batch,
+        sessions: new Map([[session, fileUrl]]),
+      });
+      await newRecord.save();
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Academic calendar uploaded successfully",
+      link: fileUrl,
     });
-
-
-    res.json(response.data);
   } catch (error) {
     console.error("Upload error:", error.message);
     res.status(500).json({
