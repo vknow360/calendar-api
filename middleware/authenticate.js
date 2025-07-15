957a6ee077acebc2d3592f8d27943b0995ed321f
Sunny Gupta <vknow360@gmail.com>
2025-07-15 19:19:54 +0530

    Add upload endpoint

---
+++ middleware/authenticate.js
@@ -0,0 +1,61 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const isAdmin = function (req, res, next) {
+  verifyToken(req, res, () => {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized to perform this action",
+      });
+    }
+  });
+};
+
+const verifyToken = function (req, res, next) {
+  const authHeader = req.headers.authorization;
+  console.log("Authorization Header:", authHeader);
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    console.log("Extracted Token:", token);
+    console.log(process.env.JWT_SEC);
+    jwt.verify(token, process.env.JWT_SEC, function (err, user) {
+      if (err) {
+        console.error("JWT Verification Error:", err);
+        return res.status(401).send({
+          success: false,
+          message: "Token Expired. Login again.",
+          authenticated: false,
+        });
+      }
+
+      req.user = user;
+      console.log("Decoded User:", user);
+      next();
+    });
+  } else {
+    console.log("No Authorization Header");
+    return res.status(401).send({
+      success: false,
+      authenticated: false,
+      message: "Session Expired. Please login again.",
+    });
+  }
+};
+
+const verifyTokenAndAuthorization = function (req, res, next) {
+  verifyToken(req, res, function () {
+    if (req.user.id === req.params.id) {
+      next();
+    } else {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized",
+      });
+    }
+  });
+};
+
+module.exports = { isAdmin, verifyToken, verifyTokenAndAuthorization };
--- routes/calendar.route.js
+++ routes/calendar.route.js
@@ -1,30 +1,62 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const axios = require("axios");
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 const router = express.Router();
 
-const Calendar = require('../models/calendar.model');
+const Calendar = require("../models/calendar.model");
 
-router.get('/', async (req, res) => {
-    const {course, session} = req.query;
-    if(!course || !session) {
-        return res.status(400).json({ error: 'Course and session are required' });
+router.get("/", async (req, res) => {
+  const { course, session } = req.query;
+  if (!course || !session) {
+    return res.status(400).json({ error: "Course and session are required" });
+  }
+
+  try {
+    const record = await Calendar.findOne({ course });
+    if (!record) {
+      return res
+        .status(404)
+        .json({ error: "No academic calendar found for this course" });
     }
 
-    try {
-        const record = await Calendar.findOne({course});
-        if (!record) {
-            return res.status(404).json({ error: 'No academic calendar found for this course' });
-        }
-
-        const url = record.sessions.get(session);
-        if(!url) {
-            return res.status(404).json({ error: 'No calendar found for this session' });
-        }
-
-        res.status(200).json([{link: url}]);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
+    const url = record.sessions.get(session);
+    if (!url) {
+      return res
+        .status(404)
+        .json({ error: "No calendar found for this session" });
     }
+
+    res.status(200).json([{ link: url }]);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.post("/upload", upload.single("file"), async (req, res) => {
+  try {
+    const fileName = req.file.originalname;
+
+    const base64 = req.file.buffer.toString("base64");
+
+    const response = await axios.post("https://script.google.com/macros/s/AKfycbycoMpeGVr14LHh1yZc92BXSZk8kuRhpF4Mv0mi3whOqyRAg_ZXP5mAa7kQfmO1EDX5jw/exec", null, {
+      params: {
+        name: fileName,
+        data: base64,
+      },
+    });
+
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Upload error:", error.message);
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 });
 
 module.exports = router;
